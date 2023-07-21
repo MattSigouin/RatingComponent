8ea8d3fa772ea5e61291a4d5a19d374ebf199ee5
Matt Sigouin <mattsigpip@gmail.com>
2023-07-21 19:54:54 -0400

    CSS Almost complete

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,40 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div>Hello world</div>
+    <head>
+      <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap" rel="stylesheet" />
+    </head>
+
+    <main className="flex min-h-screen bg-very-dark-blue items-center flex-col justify-center font-overpass">
+      <div className="bg-dark-blue rounded-xl w-80 h-80 p-5">
+        
+          <img className="fixed" src="/icon-star.svg" alt=""></img>
+        
+        <div className="flex pt-12 text-white text-xl font-bold ">How did we do?</div>
+        <div className="text-light-grey text-xs py-2 "> Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</div>
+      <form>
+
+     {/* Need to make css file - Rating for buttons */}
+     <div className="flex space-x-4 text-light-grey py-8 justify-around ">
+        <button >1</button>
+        <button >2</button>
+        <button >3</button>
+        <button >4</button>
+        <button >5</button>
+        </div>
+
+
+        <div>
+        <button className="bg-orange rounded-full text-white py-3 w-72 text-xs font-bold">S U B M I T</button>
+        </div>
+        </form>
+
+      </div>
+      
+    </main>
+    
+
+
   </React.StrictMode>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'white' : 'hsl(0, 0%, 100%)',
+        'medium-grey' : 'hsl(216, 12%, 54%)',
+        'dark-blue' : 'hsl(213, 19%, 18%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)',
+        'orange' : 'hsl(25, 97%, 53%)',
+      },
+      fontFamily: {
+        overpass: ['Overpass', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
