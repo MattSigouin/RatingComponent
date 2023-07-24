a06a68a2b49c2664a9bd5fe400f57efc90f8e6dc
Matt Sigouin <mattsigpip@gmail.com>
2023-07-24 18:02:03 -0400

    Finished 85% of styling. Added State for rating buttons.

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,35 +1,47 @@
 import styles from './Rating.module.css'
+import {useState} from 'react'
 
 export function Rating(){ 
+    const [selectedRating, setSelectedRating] = useState<number>()
+
+    function handleRatingClicked(rating: number) {
+        setSelectedRating(rating)
+    }
+
+
 
 return <div>
-<head>
-<link href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap" rel="stylesheet" />
-</head>
-
+    
 <main className="flex min-h-screen bg-very-dark-blue items-center flex-col justify-center font-overpass">
-<div className="bg-dark-blue rounded-xl w-80 h-80 p-5">
+<div className={styles.card}>
   
-    <img className="fixed" src="/icon-star.svg" alt=""></img>
+    <img className={styles.star} src="/icon-star.svg" alt=""></img>
   
-  <h1 className="flex pt-12 text-white text-xl font-bold ">How did we do?</h1>
-  <p className="text-light-grey text-xs py-2 "> Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-<form>
+  <h1 className={styles.header}>
+    How did we do?
+    </h1>
 
-{/* Need to make css file - Rating for buttons */}
+  <p className={styles.description}> 
+     Please let us know how we did with your support request.
+    All feedback is appreciated to help us improve our offering!
+  </p>
+
+{selectedRating}
+
 <div className={styles.group}>
-  <button >1</button>
-  <button >2</button>
-  <button >3</button>
-  <button >4</button>
-  <button >5</button>
-  </div>
+    {[1,2,3,4,5].map((rating) =>(
+        <button 
+    onClick={() => handleRatingClicked(rating)}
+    className={styles.rating}>
+        {rating}</button>
+    ))}
+</div>
 
 
-  <div>
-  <button className="bg-orange rounded-full text-white py-3 w-72 text-xs font-bold">S U B M I T</button>
-  </div>
-  </form>
+<div>
+  <button className={styles.submit}>SUBMIT</button>
+</div>
+  
 
 </div>
 
