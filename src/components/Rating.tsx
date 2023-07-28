4153b6bd64c2b95fe69cf538a69463b60573b716
Matt Sigouin <mattsigpip@gmail.com>
2023-07-28 14:08:48 -0400

    Added thank you page and styling.

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,20 +1,38 @@
 import styles from './Rating.module.css'
-import {useState} from 'react'
+import { useState } from 'react'
 
 export function Rating(){ 
     const [selectedRating, setSelectedRating] = useState<number>()
+    const [isSubmitted, setIsSubmitted] = useState(false)
 
     function handleRatingClicked(rating: number) {
         setSelectedRating(rating)
     }
 
+function handleFormSubmitted(e: React.FormEvent) {
+  e.preventDefault();
+  setIsSubmitted(true);
+}
 
 
-return <div>
+return isSubmitted ? (
+<div className={styles.thankYouCard}>
+  <img src="/thanks.svg" alt=""></img>
+  <p className={styles.selected}>You selected {selectedRating} out of 5</p>
+
+  <h1 className={styles.header}>
+    Thank you!
+    </h1>
+
+  <p className={styles.description}> 
+     We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
+  </p>
+</div>)
+:
+(
     
-<main className="flex min-h-screen bg-very-dark-blue items-center flex-col justify-center font-overpass">
-<div className={styles.card}>
-  
+
+<form onSubmit={handleFormSubmitted} className={styles.card}>
     <img className={styles.star} src="/icon-star.svg" alt=""></img>
   
   <h1 className={styles.header}>
@@ -26,25 +44,25 @@ return <div>
     All feedback is appreciated to help us improve our offering!
   </p>
 
-{selectedRating}
 
 <div className={styles.group}>
+  {/*Changed buttons to be dynamic by mapping an array. Can change number of ratings if wanted */}
     {[1,2,3,4,5].map((rating) =>(
         <button 
+    type="button"
     onClick={() => handleRatingClicked(rating)}
     className={styles.rating}>
-        {rating}</button>
+        {rating}
+        </button>
     ))}
 </div>
 
 
 <div>
-  <button className={styles.submit}>SUBMIT</button>
+  <button disabled={selectedRating === undefined} className={styles.submit}>SUBMIT</button>
 </div>
   
 
-</div>
+</form>
 
-</main>
-</div>
-}
+)}
